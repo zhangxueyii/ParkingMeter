640235889a62eb598f65f08cdfb114f3e6d2d0c1
Richard Zhang <rizhang@rizhang.local>
2018-07-11 18:07:21 +0800

    adjust button style.

---
+++ MyFirstApp/MyFirstApp/DesignableButton.swift
@@ -0,0 +1,21 @@
+//
+//  DesignableButton.swift
+//  MyFirstApp
+//
+//  Created by Richard Zhang on 2018/7/11.
+//  Copyright © 2018年 Richard Zhang. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableButton: UIButton {
+
+    @IBInspectable var fillColor: UIColor = UIColor.blue
+    @IBInspectable var cornerRadius : CGFloat = 0
+    
+    override func draw(_ rect: CGRect) {
+        layer.cornerRadius = 0
+        layer.backgroundColor = fillColor.cgColor
+    }
+}
